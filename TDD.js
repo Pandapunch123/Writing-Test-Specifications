c50f26c66e4835dc4dd23ce6fe570d7fd5c2a49b
Torra Tran <pandapunch@Torras-MacBook-Air.local>
2023-05-08 16:47:45 -0700

    done

---
+++ TDD.js
@@ -0,0 +1,21 @@
+//A function called "multiplication" that returns the product of the two input numbers.
+//function multiplication(a, b) {3, 4}
+//results in 12
+//Expect multiplication(3, 4) to be a number
+//Expect multiplication(3, 4) to be equal to 12
+//Expect multiplication("b", 4) to be an error
+
+//A function called "concatOdds" takes two arrays of integers as arguments. It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
+//Example: concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1])
+//result in [-1, 1, 3, 9, 15]
+//Expect addition(2, 3) to be a number
+//Expect addition(2, 3) to be equal to 5
+//Expect addition("a", 3) to be an error
+
+//A shopping cart checkout feature that allows a user to check out as a guest (without an account), or as a logged-in user. They should be allowed to do either, but should be asked if they want to create an account or log in if they check out as a guest.
+//What should happen if the cart is empty?
+//They should be able to leave as a guest, but should be asked if they want to create an account or log in, just in case they will return with a full cart later on.
+//What needs to be shown to the user at each step of check out?
+//Verifaction of how many items are in the cart?
+//What behaviors of this feature does the prompt miss or gloss over?
+//Technology is not perfect and some people may try sneak a small item out in their clothes rather then put it in the shopping cart.
